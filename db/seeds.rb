657a56063d0d7d64a34fe0516e7e4fe4472e338b
Maksym Dronov <simbanyan98@gmail.com>
2023-05-17 20:32:33 +0300

    Created actions and views, minimum style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do |l|
+  puts "Creating articles #{l}"
+  article = Article.new(title: Faker::Books::Lovecraft.tome, content:Faker::Books::Lovecraft.sentence(word_count: 3))
+  article.save
+end
+puts 'All done!'
